33f3fcafdf415453555b15e4eaa8087bfa87b4b8
Peter Gifford <peterjgifford@gmail.com>
2015-06-19 16:12:03 -0500

    Gulp/SCSS updates

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,36 @@ var gulp = require('gulp'),
 gutil = require('gulp-util'),
 sass = require('gulp-sass'),
 compass = require('gulp-compass'),
-concat = require('gulp-concat')
+livereload = require('gulp-livereload'),
+concat = require('gulp-concat'),
+autoprefixer = require('gulp-autoprefixer');
 
 var sassSources = ['components/sass/style.scss'];
-gulp.task('log', function() {
-  //gutil.log("TEST LOG");
-});
 
 gulp.task('sass', function () {
   gulp.src(sassSources)
-  .pipe(compass({
-    sass: 'components/sass',
-    images: 'public/images',
-    style: 'expanded'
-  }))
-  .on('error', gutil.log)
-  .pipe(gulp.dest('public/css'));
-
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(compass({
+      sass: 'components/sass',
+      images: 'public/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('public/css'))
+    .pipe(livereload());
 });
 
 gulp.task('sass:watch', function () {
+  livereload.listen();
   gulp.watch(sassSources, ['sass']);
 });
 
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(sassSources, ['sass']);
+});
+
+gulp.task('default', ['watch', 'sass']);
