b57e4d105a21e590566c8119cfaa7204630a9f84
ndoz <ndoz78@hotmail.com>
2015-06-19 14:31:33 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var concat = require('gulp-concat');
+var autoprefixer = require('autoprefixer-core');
+var postcss = require('gulp-postcss');
 
 gulp.task('sass', function() {
     return gulp.src('public/scss/**/*.scss')
         .pipe(sass())
+        .pipe(postcss([autoprefixer({
+            browsers: ['last 2 versions']
+        })]))
         .pipe(gulp.dest('public/css/'));
+        //.pipe(concat('style.css'))
+        //.pipe(gulp.dest('public/css'));
 });
 gulp.watch('public/scss/**/*.scss', ['sass']);
 
