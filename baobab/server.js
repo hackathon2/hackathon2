bdec098b69951deaa75db55d5170f9ace6c58825
ndoz <ndoz78@hotmail.com>
2015-08-06 18:48:35 -0500

    bao cleanup

--- baobab/server.js
+++ baobab/server.js
@@ -4,3 +4,4 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);
+ console.log('BaoBab folder listening at http://localhost:3000');
